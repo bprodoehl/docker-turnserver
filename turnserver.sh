6328e4ab45790faa12bcade1de15c6cf465a9266
Brian Prodoehl <bprodoehl@connectify.me>
2014-08-29 14:29:59 -0400

    Add listening port parameter.

--- turnserver.sh
+++ turnserver.sh
@@ -5,11 +5,16 @@ then
     EXTERNAL_IP=`curl icanhazip.com 2> /dev/null`
 fi
 
+if [ -z $PORT ]
+then
+    PORT=3478
+fi
+
 if [ ! -e /tmp/turnserver.configured ]
 then
     echo external-ip=$EXTERNAL_IP > /etc/turnserver.conf
+    echo listening-port=$PORT >> /etc/turnserver.conf
     touch /tmp/turnserver.configured
 fi
 
 exec /usr/bin/turnserver >>/var/log/turnserver.log 2>&1
-
